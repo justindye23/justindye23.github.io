dad39039ca95815970865519efe920452af912d3
justindye23 <35241484+justindye23@users.noreply.github.com>
2018-02-24 15:47:30 -0700

    updates

--- cit230/assignments/lesson-7/scripts/windchill.js
+++ cit230/assignments/lesson-7/scripts/windchill.js
@@ -2,9 +2,9 @@ var t = 50;
 var s = 10;
 var f = getWindChillFactor(t,s);
 
-document.getElementById('windSpeed').innerHTML = s;
+document.getElementById("windSpeed").innerHTML = s;
 document.getElementById("temp").innerHTML = t;
-document.getElementById("wind").innerHTML = f;
+document.getElementById("windChillFactor").innerHTML = f;
 
 console.log(f);
 
