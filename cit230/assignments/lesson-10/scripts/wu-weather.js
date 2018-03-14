524e2abc91eed2877a3260024480fbf0755930e1
justindye23 <35241484+justindye23@users.noreply.github.com>
2018-03-13 21:16:59 -0600

    updates

--- cit230/assignments/lesson-10/scripts/wu-weather.js
+++ cit230/assignments/lesson-10/scripts/wu-weather.js
@@ -1,17 +1,20 @@
-var requestURL = 'http://api.wunderground.com/api/1214f7b6c790d58f/geolookup/conditions/q/MN/Franklin.json';
+var requestURL = 'https://api.wunderground.com/api/1214f7b6c790d58f/geolookup/conditions/q/MN/Franklin.json';
 
 var request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
 
-request.onload = function() {
-    var superHeroes = request.response;
-    populateHeader(superHeroes);
-    showHeroes(superHeroes);
+request.onload = function () {
+    var franklinWeather = request.response;
+
+    //franklin response.version.termsofservice
+    document.getElementById("frankWeather").innerHTML = franklinWeather.current_observation.weather;
+
+    document.getElementById("frankTemp").innerHTML = franklinWeather.current_observation.temperature_string;
+    document.getElementById("frankWind").innerHTML = franklinWeather.current_observation.wind_mph;
+    document.getElementById("frankIcon").innerHTML = franklinWeather.current_observation.icon;
 }
 
-franklin response.version.termsofservice
-var weather = franklin.currentobservation.temperature_string;
 
-document.getElementById("weather").innerHTML = weather;
+
--- cit230/assignments/lesson-9/scripts/wu-weather.js
+++ cit230/assignments/lesson-9/scripts/wu-weather.js
@@ -2,15 +2,19 @@ var requestURL = 'https://api.wunderground.com/api/1214f7b6c790d58f/geolookup/co
 
 var request = new XMLHttpRequest();
 request.open('GET', requestURL);
+request.responseType = 'json';
 request.send();
 
-request.onload = function() {
-    var franklinWeather = JSON.parse(request.responseText);
+request.onload = function () {
+    var franklinWeather = request.response;
+
+    //franklin response.version.termsofservice
+    document.getElementById("frankWeather").innerHTML = franklinWeather.current_observation.weather;
+
+    document.getElementById("frankTemp").innerHTML = franklinWeather.current_observation.temperature_string;
+    document.getElementById("frankWind").innerHTML = franklinWeather.current_observation.wind_mph;
+    document.getElementById("frankIcon").innerHTML = franklinWeather.current_observation.icon;
 }
 
-//franklin response.version.termsofservice
-document.getElementById('desc').innerHTML = franklinWeather.current_observation.weather;
 
-var temperature_string = franklinWeather.estimated.temperature_string;
-document.getElementById("temperature").innerHTML = temperature_string;
 
