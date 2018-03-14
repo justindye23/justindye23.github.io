2d3289ba6168b2fe6fb22a69454ab64c800a5388
justindye23 <35241484+justindye23@users.noreply.github.com>
2018-03-13 21:25:49 -0600

    updates

--- cit230/assignments/lesson-10/scripts/towndata.js
+++ cit230/assignments/lesson-10/scripts/towndata.js
@@ -11,48 +11,56 @@ request.onload = function () {
     var towns = request.response;
 
 
-    var townName = towns[0].name;
-    document.getElementById("franklinData").innerHTML = "townName";
+    var franklin = towns.towns[0];
 
-    var moto = towns[0].moto
-    document.getElementById("frankMoto").innerHTML = "moto";
 
-    var yearFounded = towns[0].yearFounded
-    document.getElementById("franklinData").innerHTML = "yearFounded";
+    var townName = franklin.name;
+    document.getElementById("frankName").innerHTML = townName;
 
-    var currentPopulation = towns[0].currentPopulation
-    document.getElementById("franklinData").innerHTML = currentPopulation;
+    var motto = franklin.motto;
+    document.getElementById("frankMotto").innerHTML = motto;
 
-    var averageRainfall = towns[0].averageRainfall
-    document.getElementById("franklinData").innerHTML = averageRainfall;
+    var yearFounded = franklin.yearFounded
+    document.getElementById("frankYear").innerHTML = yearFounded;
 
-    var townName = towns[1].name;
-    document.getElementById("greenvilleData").innerHTML = "townName";
+    var currentPopulation = franklin.currentPopulation
+    document.getElementById("frankPopulation").innerHTML = currentPopulation;
 
-    var moto = towns[1].moto
-    document.getElementById("greenvilleData").innerHTML = "moto";
+    var averageRainfall = franklin.averageRainfall
+    document.getElementById("frankRain").innerHTML = averageRainfall;
 
-    var yearFounded = towns[1].yearFounded
-    document.getElementById("greenvilleData").innerHTML = "yearFounded";
 
-    var currentPopulation = towns[1].currentPopulation
-    document.getElementById("greenvilleData").innerHTML = "currentPopulation";
+    var greenville = towns.towns[1];
 
-    var averageRainfall = towns[1].averageRainfall
-    document.getElementById("greenvilleData").innerHTML = "averageRainfall";
+    var townName = greenville.name;
+    document.getElementById("greenName").innerHTML = townName;
 
-    var townName = towns[3].name;
-    document.getElementById("springfieldData").innerHTML = "townName";
+    var motto = greenville.motto;
+    document.getElementById("greenMotto").innerHTML = motto;
 
-    var moto = towns[3].moto
-    document.getElementById("springfieldData").innerHTML = "moto";
+    var yearFounded = greenville.yearFounded
+    document.getElementById("greenYear").innerHTML = yearFounded;
 
-    var yearFounded = towns[3].yearFounded
-    document.getElementById("springfieldData").innerHTML = "yearFounded";
+    var currentPopulation = greenville.currentPopulation
+    document.getElementById("greenPopulation").innerHTML = currentPopulation;
 
-    var currentPopulation = towns[3].currentPopulation
-    document.getElementById("springfieldData").innerHTML = "currentPopulation";
+    var averageRainfall = greenville.averageRainfall
+    document.getElementById("greenRain").innerHTML = averageRainfall;
 
-    var averageRainfall = towns[3].averageRainfall
-    document.getElementById("springfieldData").innerHTML = "averageRainfall";
+    var springfield = towns.towns[3];
+
+    var townName = springfield.name;
+    document.getElementById("springName").innerHTML = townName;
+
+    var motto = springfield.motto
+    document.getElementById("springMotto").innerHTML = motto;
+
+    var yearFounded = springfield.yearFounded
+    document.getElementById("springYear").innerHTML = yearFounded;
+
+    var currentPopulation = springfield.currentPopulation
+    document.getElementById("springPopulation").innerHTML = currentPopulation;
+
+    var averageRainfall = springfield.averageRainfall
+    document.getElementById("springRain").innerHTML = averageRainfall;
 }
