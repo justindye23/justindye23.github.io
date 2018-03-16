1455e8c4ef19f062da80a1104a8bc307b4b0255b
justindye23 <35241484+justindye23@users.noreply.github.com>
2018-03-16 15:46:51 -0600

    updates

--- cit230/assignments/lesson-10/scripts/wu-weatherG.js
+++ cit230/assignments/lesson-10/scripts/wu-weatherG.js
@@ -1,4 +1,4 @@
-var requestURL = 'https://api.wunderground.com/api/1214f7b6c790d58f/geolookup/forecast/q/MN/greenville.json';
+var requestURL = 'https://api.wunderground.com/api/1214f7b6c790d58f/geolookup/conditions/q/MN/Greenville.json';
 
 var request = new XMLHttpRequest();
 request.open('GET', requestURL);
@@ -8,9 +8,24 @@ request.send();
 request.onload = function () {
     var greenvilleWeather = request.response;
 
-    //franklin response.version.termsofservice
     document.getElementById("greenWeather").innerHTML = greenvilleWeather.current_observation.weather;
+
     document.getElementById("greenTemp").innerHTML = greenvilleWeather.current_observation.temperature_string;
     document.getElementById("greenWind").innerHTML = greenvilleWeather.current_observation.wind_mph;
-    document.getElementById("greenIcon").innerHTML = greenvilleWeather.current_observation.icon_url;
+    document.getElementById("greenIcon").src = greenvilleWeather.current_observation.icon_url;
+
+
+    requestURL = 'https://api.wunderground.com/api/1214f7b6c790d58f/geolookup/forecast/q/MN/Greenville.json';
+
+    request.open('GET', requestURL);
+    request.send();
+
+    request.onload = function () {
+        var greenvilleForecast = request.response;
+
+        greenvilleText = greenvilleForecast.forecast.txt_forecast.forecastday[0].fcttext;
+        document.getElementById("greenForecast").innerHTML = greenvilleTextText;
+    }
+
 }
+
--- cit230/assignments/lesson-10/scripts/wu-weatherS.js
+++ cit230/assignments/lesson-10/scripts/wu-weatherS.js
@@ -1,4 +1,4 @@
-var requestURL = 'https://api.wunderground.com/api/1214f7b6c790d58f/geolookup/conditions/q/MN/springfield.json';
+var requestURL = 'https://api.wunderground.com/api/1214f7b6c790d58f/geolookup/conditions/q/MN/Springfield.json';
 
 var request = new XMLHttpRequest();
 request.open('GET', requestURL);
@@ -6,11 +6,26 @@ request.responseType = 'json';
 request.send();
 
 request.onload = function () {
-    var springfieldWeather = request.response;
+    var franklinWeather = request.response;
 
-    document.getElementById("frankWeather").innerHTML = springfieldWeather.current_observation.weather;
+    document.getElementById("springWeather").innerHTML = franklinWeather.current_observation.weather;
 
-    document.getElementById("frankTemp").innerHTML = springfieldWeather.current_observation.temperature_string;
-    document.getElementById("frankWind").innerHTML = springfieldWeather.current_observation.wind_mph;
-    document.getElementById("frankIcon").innerHTML = springfieldWeather.current_observation.icon_url;
+    document.getElementById("springTemp").innerHTML = franklinWeather.current_observation.temperature_string;
+    document.getElementById("springWind").innerHTML = franklinWeather.current_observation.wind_mph;
+    document.getElementById("springIcon").src = franklinWeather.current_observation.icon_url;
+
+
+    requestURL = 'https://api.wunderground.com/api/1214f7b6c790d58f/geolookup/forecast/q/MN/Springfield.json';
+
+    request.open('GET', requestURL);
+    request.send();
+
+    request.onload = function () {
+        var springfieldForecast = request.response;
+
+        springText = springfieldForecast.forecast.txt_forecast.forecastday[0].fcttext;
+        document.getElementById("springForecast").innerHTML = springText;
+    }
+
 }
+
