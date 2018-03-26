e3f151a4a547a08926c79d56123a272bebcbea01
justindye23 <35241484+justindye23@users.noreply.github.com>
2018-03-26 16:08:22 -0600

    github updates

--- cit230/assignments/lesson-11/intro-jquery-files/lesson4/js/dropDownMenu.js
+++ cit230/assignments/lesson-11/intro-jquery-files/lesson4/js/dropDownMenu.js
@@ -1,12 +1,12 @@
-    $(function(){
-     $('dropdownMenu > li').click( function(){
-        $(this).children("ul").slideDown(200);
-        }, function(){
-          $(this).children("ul").slideUp(200);
+    $(function () {
+                // $('dropdownMenu > li').click( function(){
+                //  $(this).children("ul").slideDown(200);
+                //    }, function(){
+                //    $(this).children("ul").slideUp(200);
 
-     })
+                // })
 
-    // $('#dropDownMenu > li').hover(function() {
-       // $(this).children("ul").slideToggle(200);
+                $('.dropDownMenu > li').hover(function () {
+                    $(this).children("ul").slideToggle(200);
+                })
     })
-
